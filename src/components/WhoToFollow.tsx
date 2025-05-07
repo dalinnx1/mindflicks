c4304d626caa7712a67fe29508f68f507f8df2ad
dalinnx1 <dalinnx1@gmail.com>
2025-05-07 14:26:54 +0300

    minor fix

--- src/components/WhoToFollow.tsx
+++ src/components/WhoToFollow.tsx
@@ -43,7 +43,7 @@ export default async function WhoToFollow() {
                                     </p>
                                 </div>
                             </div>
-                            <FollowButton userId={user._id.toString()} />
+                            <FollowButton userId={user?._id.toString()} />
                         </div>
                     ))}
                 </div>
